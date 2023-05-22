e5e48b1345d695d1c1d65a014ad666566a85f90d
dhikaaaa <dhikabahri4@gmail.com>
2023-05-22 16:01:52 +0700

    refactor : add migration, model, and controller for backend

--- database/migrations/2023_05_22_084844_create_menus_table.php
+++ database/migrations/2023_05_22_084844_create_menus_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('menus', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('role_id');
+            $table->string('menu_name');
+            $table->boolean('is_show')->default(1);
+            $table->foreignId('user_id');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('menus');
     }
 };
---
+++ database/migrations/2023_05_22_084923_create_contents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->string('content_name');
+            $table->string('content-text')->nullable();
+            $table->string('content_image')->nullable();
+            $table->string('content_video')->nullable();
+            $table->string('content_link')->nullable();
+            $table->boolean('is_show')->default(1);
+            $table->foreignId('menu_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+};
